307b51ca679ee25acb181dc4456a613a7316795a
Xingchen Qi <XingchenQi@users.noreply.github.com>
2023-09-05 04:02:55 -0500

    remove duplicate orders and change shuffle logic

--- src/main/edu/utexas/ece/sa/tools/parser/ParserMojo.java
+++ src/main/edu/utexas/ece/sa/tools/parser/ParserMojo.java
@@ -232,22 +232,13 @@ public class ParserMojo extends AbstractParserMojo {
     protected void superExecute() {
         super.execute();
     }
-    
-    private void checkTestsOrder(HashMap<String,List<String>> splitTests){
-        System.console().printf("print tests\n");
 
-        splitTests.forEach((key, value) -> System.out.println(key + " " + value));
-        List<String> allTests=new ArrayList<>();
-        for(String newClass:splitTests.keySet()){
-            List<String> testsInOrder=splitTests.get(newClass);
-            allTests.addAll(testsInOrder);
-            System.out.println(testsInOrder);
-            Map<String, TestResult> innerMap = this.runner.runList(testsInOrder).get().results();
-            System.out.println("RUNNING RESULTS WITH SAME ORDER: " + innerMap);
-        }
-        System.out.println("ALL TESTS ORDER: " + allTests);
-        Map<String, TestResult> newResultsInOrder = this.runner.runList(allTests).get().results();
-        System.out.println("RUNNING RESULTS WITH ALL TESTS IN ORDER: " + newResultsInOrder);
+    private void shuffleAllTests(List<String> originalTests,Integer threshold){
+        List<String> newTests=new LinkedList<>(originalTests);
+        List<String> bestOrder=new LinkedList<>(originalTests);
+        Set<List<String>> setOfLists = new HashSet<>();
+        setOfLists.add(new LinkedList<>(originalTests));
+        boolean hasBetterOrder=false;
         for(int i=0;i<shuffleTimes;i++){
             // Generate a seed and print it
             long generatedSeed = new Random().nextLong();
@@ -256,23 +247,108 @@ public class ParserMojo extends AbstractParserMojo {
             // Use the generated seed for reproducibility
             Random random = new Random(generatedSeed);
 
-            Collections.shuffle(allTests,random);
-            System.out.println("ALL TESTS ORDER: " + allTests);
-            Map<String, TestResult> newResultsRandom = this.runner.runList(allTests).get().results();
-            System.out.println("RUNNING RESULTS WITH ALL TESTS SHUFFLE: " + newResultsRandom);
-            boolean foundFail=false;
+            Collections.shuffle(newTests,random);
+            if(setOfLists.contains(newTests)){
+                System.out.println("CURRENT TESTS ORDER EXISTS, SKIPPING.....");
+                continue;
+            }
+            setOfLists.add(new LinkedList<>(newTests));
+            System.out.println("NEW TESTS ORDER: " + newTests);
+            Map<String, TestResult> newResultsRandom = this.runner.runList(newTests).get().results();
+            System.out.println("RUNNING RESULTS WITH NEW ORDER: " + newResultsRandom);
+            Integer failedCnt=0;
             for (String key : newResultsRandom.keySet()) {
                 TestResult testResult = newResultsRandom.get(key);
                 if (testResult.result().toString().equals("FAILURE")) {
-                    System.out.println("FOUND FAILURE IN CURRENT ORDER! "+key);
-                    System.out.println("Please remain previous order.");
-                    foundFail=true;
-                    break;
+                    failedCnt++;
                 }
                 if (testResult.result().toString().equals("ERROR")) {
-                    System.out.println("FOUND ERROR IN CURRENT ORDER! "+key);
-                    System.out.println("Please remain previous order.");
-                    foundFail=true;
+                    failedCnt++;
+                }
+                if (testResult.result().toString().equals("SKIPPED")) {
+                    System.out.println("TESTS SKIPPED!!!");
+                    continue;
+                }
+            }
+            if(failedCnt<threshold){
+                hasBetterOrder=true;
+                bestOrder=new LinkedList<>(newTests);
+                System.out.println("FOUND BETTER ORDER WITH MORE PASSES!");
+                System.out.println("NEW TESTS ORDER: " + newTests);
+                threshold=failedCnt;
+            }
+        }
+        if(hasBetterOrder){
+            System.out.println("THERE IS A BETTER ORDER THAN ORIGINAL!");
+        }else{
+            System.out.println("NO BETTER ORDER THAN ORIGINAL!");
+        }
+        System.out.println("THE BEST ORDER IN THIS CLASS IS: ");
+        System.out.println(bestOrder);
+    }
+    
+    private void checkTestsOrder(HashMap<String,List<String>> splitTests){
+        System.console().printf("=====START CHECKING TESTS ORDER AFTER SPLIT=====\n");
+
+        splitTests.forEach((key, value) -> System.out.println(key + " " + value));
+        List<String> allTests=new ArrayList<>();
+        List<String> allClasses=new ArrayList<>();
+        for(String newClass:splitTests.keySet()){
+            allClasses.add(newClass);
+            List<String> testsInOrder=splitTests.get(newClass);
+            allTests.addAll(testsInOrder);
+//            System.out.println(testsInOrder);
+//            Map<String, TestResult> innerMap = this.runner.runList(testsInOrder).get().results();
+//            System.out.println("RUNNING RESULTS WITH SAME ORDER: " + innerMap);
+        }
+        HashSet<List<String>> allOrders=new HashSet<>();
+        allOrders.add(new ArrayList<>(allTests));
+        System.out.println("ALL TESTS ORDER: " + allTests);
+        Map<String, TestResult> newResultsInOrder = this.runner.runList(allTests).get().results();
+        System.out.println("RUNNING RESULTS WITH ALL TESTS IN ABOVE ORDER: " + newResultsInOrder);
+        boolean foundFail=false;
+        for(int i=0;i<shuffleTimes;i++){
+            // Generate a seed and print it
+            long generatedSeed = new Random().nextLong();
+            System.out.println("Generated seed: " + generatedSeed);
+
+            // Use the generated seed for reproducibility
+            Random random = new Random(generatedSeed);
+
+            Collections.shuffle(allClasses,random);
+
+            System.out.println("NEW CLASSES ORDER: " + allClasses);
+            for(int j=0;j<shuffleTimes;j++){
+                List<String> gatherAllTests=new ArrayList<>();
+                for(String curClass:allClasses){
+                    List<String> testsByClass=splitTests.get(curClass);
+                    Collections.shuffle(testsByClass,random);
+                    gatherAllTests.addAll(testsByClass);
+                }
+                if(allOrders.contains(gatherAllTests)){
+                    System.out.println("CURRENT TESTS ORDER EXISTS, SKIPPING.....");
+                    continue;
+                }
+                allOrders.add(gatherAllTests);
+                System.out.println("NEW TESTS ORDER: " + gatherAllTests);
+                Map<String, TestResult> newResultsRandom = this.runner.runList(gatherAllTests).get().results();
+                System.out.println("RUNNING RESULTS WITH ALL TESTS SHUFFLE: " + newResultsRandom);
+
+                for (String key : newResultsRandom.keySet()) {
+                    TestResult testResult = newResultsRandom.get(key);
+                    if (testResult.result().toString().equals("FAILURE")) {
+                        System.out.println("FOUND FAILURE IN CURRENT ORDER! "+key);
+                        foundFail=true;
+                        break;
+                    }
+                    if (testResult.result().toString().equals("ERROR")) {
+                        System.out.println("FOUND ERROR IN CURRENT ORDER! "+key);
+                        foundFail=true;
+                        break;
+                    }
+                }
+                if(foundFail){
+                    System.out.println("==========FOUND FAILURES/ERRORS IN CURRENT ABOVE ORDER! PLEASE REMAIN THE ORIGINAL ORDER!==========");
                     break;
                 }
             }
@@ -281,6 +357,8 @@ public class ParserMojo extends AbstractParserMojo {
             }
         }
     }
+
+
 
     @Override
     public void execute() {
@@ -413,6 +491,7 @@ public class ParserMojo extends AbstractParserMojo {
                             System.out.println(map);
                             Set<String> failedTests = new HashSet<>();
                             obtainLastTestResults(map, failedTests);
+                            shuffleAllTests(testsForNewClass,failedTests.size());
                             for (String failedTest : failedTests) {
                                 String formalFailedTest = failedTest;
                                 if (failedTest.contains("#") || failedTest.contains("()")) {
